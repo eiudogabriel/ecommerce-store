049e3db9d557e367bb02c6de578d0dca40e396e1
Eiudo Gabriel <eiudogabriel@gmail.com>
2023-08-11 19:38:12 -0300

    Featured Products page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,25 @@
-const HomePage = () => {
-  return ( 
-    <div>home page</div>
-   );
-}
- 
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("3c276275-fed0-4417-8baf-54553cc75667");
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Bombando!" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
 export default HomePage;
