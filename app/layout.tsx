aeecea8a980350b84817edff0e6dbf2b76a595e3
Eiudo Gabriel <eiudogabriel@gmail.com>
2023-08-18 17:48:26 -0300

    Preview Modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,31 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Space_Grotesk } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Space_Grotesk } from "next/font/google";
 
-import Footer from '@/components/footer'
-import Navbar from '@/components/navbar'
+import Footer from "@/components/footer";
+import Navbar from "@/components/navbar";
+import ModalProvider from "@/providers/modal-provider";
 
-const font = Space_Grotesk({ subsets: ['latin'] })
+const font = Space_Grotesk({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'ECommerce store',
-  description: 'Ecommerce store',
-}
+  title: "ECommerce store",
+  description: "Ecommerce store",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={font.className}>
+        <ModalProvider />
         <Navbar />
         {children}
         <Footer />
-        </body>
+      </body>
     </html>
-  )
+  );
 }
--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -2,12 +2,14 @@
 
 import Image from "next/image";
 import { Expand, ShoppingCart } from "lucide-react";
+import { MouseEventHandler } from "react";
 
 import { Product } from "@/types";
 
 import IconButton from "@/components/ui/icon-button";
 import Currency from "@/components/ui/currency";
 import { useRouter } from "next/navigation";
+import usePreviewModal from "@/hooks/use-preview-modal";
 
 interface ProductCardProps {
   data: Product;
@@ -15,10 +17,18 @@ interface ProductCardProps {
 
 const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
   const router = useRouter();
+  const previewModal = usePreviewModal();
 
   const handleClick = () => {
     router.push(`/product/${data?.id}`);
   };
+
+  const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+
+    previewModal.onOpen(data);
+  };
+
   return (
     <div
       onClick={handleClick}
@@ -35,7 +45,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
         <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
           <div className="flex gap-x-6 justify-center">
             <IconButton
-              onClick={() => {}}
+              onClick={onPreview}
               icon={<Expand size={20} className="text-gray-600" />}
             />
             <IconButton
