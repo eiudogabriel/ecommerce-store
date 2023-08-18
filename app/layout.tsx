c25a8bb382b57e194e553432a9f2b256513c8932
Eiudo Gabriel <eiudogabriel@gmail.com>
2023-08-18 17:55:35 -0300

    Toast Provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,9 @@ import { Space_Grotesk } from "next/font/google";
 
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
+
 import ModalProvider from "@/providers/modal-provider";
+import ToastProvider from "@/providers/toast-provider";
 
 const font = Space_Grotesk({ subsets: ["latin"] });
 
@@ -22,6 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ModalProvider />
+        <ToastProvider />
         <Navbar />
         {children}
         <Footer />
